0d0dfc03e297524b551a26ff71e8e6b5b2004c46
Fei <unshi@outlook.com>
2019-10-13 15:39:41 -0400

    mvvm

---
+++ isprime/isprime/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+namespace isprime
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM(); //建立新的对象
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm; //绑定vm的数据来源
+        }
+
+        public bool isPrime(int num) //以6的倍数判断是否是素数
+        {
+            if (num == 2 || num == 3)
+            {
+                return true;
+            }
+            if (num % 6 != 1 && num % 6 != 5)
+            {
+                return false;
+            }
+            int tmp = Convert.ToInt32(Math.Sqrt(Convert.ToDouble(num)));
+            for (int i = 5; i <= tmp; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int size = vm.TargetNum; //获取对象的内容
+
+            for (int i = 2; i <= size; i++)
+            {
+                if (isPrime(i) == true)
+                {
+                    primeResult.Items.Add(i);
+                }
+            }
+        }
+    }
+}
---
+++ isprime/isprime/VM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isprime
+{
+    public class PropertyChangedBase : INotifyPropertyChanged //创建一个基类监听PropertyChange事件
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName) //建立一个函数当property有变化时更新event到新的状态
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+
+   
+    public class VM: PropertyChangedBase //建立viewmodel继承propertychange基类
+    {
+        private int targetNum;
+        public int TargetNum //当targetnum的set有变化时更新状态
+        {
+            set { targetNum = value;OnPropertyChanged("TargetNum"); } 
+            get { return targetNum; }
+        }
+    }
+}
